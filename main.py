b2099a037019c2c99199432204a199f03265cb6b
Artsiom Halachkin <xhalachk@mendelu.cz>
2024-03-12 19:52:32 +0100

    version3

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import random
 import time
-
 board = [
     ["|", "|", "|"],
     ["|", "|", "|"],
@@ -9,23 +8,19 @@ board = [
 state = False
 Player1Char = " "
 Player2Char = " "
-invalid_choices = []
 
 def step_machine_choice():
-    global invalid_choices
     print("\nPlayer2 move..")
-    time.sleep(2)
+    time.sleep(1)
     choice = [random.randint(0, 2) for i in range(2)]
-    while choice in invalid_choices:
+    while board[choice[0]][choice[1]] == Player1Char:
         choice = [random.randint(0, 2) for i in range(2)]
-
-    invalid_choices.append(choice)
     return choice
 
 def choice_side():
     global Player1Char
     global Player2Char
-    inpt = input("Hello please choice sid [0] or [+]: ")
+    inpt = input("Ahoj, vyber si [0] nebo [+]: ")
     Player1Char = inpt
     if inpt == "0":
         Player2Char = "+"
@@ -38,37 +33,39 @@ def print_Board(board):
         print()
 
 def step_input_choice():
-    global invalid_choices
-    choice = [int(i) for i in input("Enter step: ").split(",")]
-    if choice in invalid_choices:
-        print("\ninvalid input")
-    else:
-        invalid_choices.append(choice)
-        return choice
+    choice = [int(i) for i in input("Enter: ").split(",")]
+    while board[choice[0]][choice[1]] == Player2Char:
+        choice = [int(i) for i in input("je obsazeno napiste jeste: ").split(",")]
+    return choice
 
 def input_handling(input_choice, char):
     global board
     board[input_choice[0]][input_choice[1]] = char
 
-def check_win():
-    global state
+def check_for_plr1_win():
     for i in range(3):
-        if board[i][0] == board[i][1] == board[i][2] != '|':
-            state = True
-            win_message(board[i][1])
-        elif board[i][0] == board[i][1] == board[i][2] != '|':
-            state = True
-            win_message(board[i][1])
-        elif board[0][0] == board[1][1] == board[2][2] != '|':
-            state = True
-            win_message(board[1][1])
-        elif board[0][0] == board[1][1] == board[2][2] != '|':
-            state = True
-            win_message(board[1][1])
-
-def win_message(winChar):
-    print(f"\n{winChar} vyhrál!\n")
-
+        if board[i][0] == board[i][1] == board[i][2] == Player1Char:
+            return True
+        elif board[i][0] == board[i][1] == board[i][2] == Player1Char:
+            return True
+        elif board[0][0] == board[1][1] == board[2][2] == Player1Char:
+            return True
+        elif board[2][2] == board[1][1] == board[0][0] == Player1Char:
+            return True
+        else:
+            return False
+def check_for_plr2_win():
+    for i in range(3):
+        if board[i][0] == board[i][1] == board[i][2] == Player2Char:
+            return True
+        elif board[i][0] == board[i][1] == board[i][2] == Player2Char:
+            return True
+        elif board[0][0] == board[1][1] == board[2][2] == Player2Char:
+            return True
+        elif board[2][2] == board[1][1] == board[0][0] == Player2Char:
+            return True
+        else:
+            return False
 
 def play():
     choice_side()
@@ -76,8 +73,14 @@ def play():
         print_Board(board)
         input_handling(step_input_choice(), Player1Char)
         input_handling(step_machine_choice(), Player2Char)
-        check_win()
-
+        if check_for_plr1_win():
+            print(f"\n{Player1Char} VYHRAL!")
+        elif check_for_plr2_win():
+            print(f"\n{Player2Char} VYHRAL!")
+        elif check_for_plr1_win() and check_for_plr2_win:
+            print("\nREMIZA!!!")
+        else:
+            continue
 
 if __name__ == '__main__':
     play()
